c7f62cd097d5e20360671571ab622a7d72cf6350
Orie Steele <orie@ohs.io>
2019-03-25 13:51:32 -0500

    app api test

---
+++ packages/element-api/src/__tests__/web3.spec.js
@@ -0,0 +1,28 @@
+const element = require("@transmute/element-lib");
+
+describe("web3", () => {
+  beforeAll(() => {
+    process.env.ELEMENT_MNEUMONIC =
+      "hazard pride garment scout search divide solution argue wait avoid title cave";
+    process.env.ELEMENT_PROVIDER = "http://localhost:8545";
+  });
+
+  it("element has web3 helper that works", async () => {
+    const web3 = element.ledger.getWeb3({
+      mneumonic: process.env.ELEMENT_MNEUMONIC,
+      providerUrl: process.env.ELEMENT_PROVIDER
+    });
+    const accounts = await web3.eth.getAccounts();
+    expect(accounts[0]).toBe("0x1E228837561e32a6eC1b16f0574D6A493Edc8863");
+  });
+
+  it("element can create new anchor contract", async () => {
+    const instance = await element.ledger.createNewContract();
+    expect(element.ledger.getContractAddress()).toBe(instance.address);
+    
+  });
+
+  afterAll(() => {
+    // web3.currentProvider.engine.stop();
+  });
+});
