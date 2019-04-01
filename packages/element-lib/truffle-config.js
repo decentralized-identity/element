45e27003212661fc347e8d38ab1c285088dba67f
Orie Steele <orie@ohs.io>
2019-04-01 12:55:44 -0500

    deployed to ropsten - updated local configs

--- packages/element-lib/truffle-config.js
+++ packages/element-lib/truffle-config.js
@@ -18,11 +18,10 @@
  *
  */
 
-// const HDWalletProvider = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const path = require('path');
+// eslint-disable-next-line
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 
 module.exports = {
   /**
@@ -45,7 +44,6 @@ module.exports = {
     development: {
       host: '127.0.0.1', // Localhost (default: none)
       port: 8545, // Standard Ethereum port (default: none)
-      network_id: '*', // Any network (default: none)
       websockets: true,
       network_id: 133700,
     },
@@ -62,14 +60,15 @@ module.exports = {
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/${infuraKey}`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    ropsten: {
+      // eslint-disable-next-line
+      provider: () => new HDWalletProvider(process.env.ELEMENT_MNEMONIC, process.env.ELEMENT_PROVIDER),
+      network_id: 3, // Ropsten's id
+      gas: 5500000, // Ropsten has a lower block limit than mainnet
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
 
     // Useful for private networks
     // private: {
