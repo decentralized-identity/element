240de9e08934910297fc1e642ddb61fdc4cc61b6
Orie Steele <orie@ohs.io>
2019-06-21 18:27:15 -0500

    lint

--- packages/element-lib/src/sidetree/sync.js
+++ packages/element-lib/src/sidetree/sync.js
@@ -1,9 +1,8 @@
 module.exports = async (sidetree) => {
   // eslint-disable-next-line
   sidetree.sync = async ({ fromTransactionTime, toTransactionTime }) => {
+    // todo: if synching, don't sync....
 
-    // if synching, don't sync....
-    
     sidetree.serviceBus.emit('element:sidetree:sync:start', {
       fromTransactionTime,
       toTransactionTime,
