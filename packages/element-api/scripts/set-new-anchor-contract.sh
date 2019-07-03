987e6150d5ded486521985d9ccf528a532591c0e
Orie Steele <orie@ohs.io>
2019-07-03 14:14:25 -0500

    update config mgmt

--- packages/element-api/scripts/set-new-anchor-contract.sh
+++ packages/element-api/scripts/set-new-anchor-contract.sh
@@ -1,3 +1,4 @@
+# make sure too lerna bootstrap first
 ADDRESS=$(cat ./node_modules/@transmute/element-lib/build/contracts/SimpleSidetreeAnchor.json| jq -r '.networks["133700"].address')
 
 echo 'changing local contract address to: ' $ADDRESS
@@ -5,4 +6,4 @@ echo 'changing local contract address to: ' $ADDRESS
 # firebase functions:config:set element.ethereum.anchor_contract_address=$ADDRESS
 
 tmp=$(mktemp)
-jq ".element.ethereum.anchor_contract_address = \"$ADDRESS\"" ./.runtimeconfig.json > "$tmp" && mv "$tmp" ./.runtimeconfig.json
+jq ".element.ethereum.anchor_contract_address = \"$ADDRESS\"" ./local.runtimeconfig.json > "$tmp" && mv "$tmp" ./local.runtimeconfig.json
