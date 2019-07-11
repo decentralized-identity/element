94a3313999d4f356d4aebcade5ad99faa4a4a63f
gjgd <gjgd@protonmail.com>
2019-07-11 13:49:43 +0100

    Update ipfs start script

--- packages/element-lib/scripts/ipfs/start.sh
+++ packages/element-lib/scripts/ipfs/start.sh
@@ -4,11 +4,13 @@ ipfs_running() {
   nc -z localhost "$IPFS_PORT"
 }
 start_ipfs() {
-  npx ipfs init > /dev/null 2>&1
-  # may need to change in future.
-  # npx ipfs config Addresses.API /ip4/0.0.0.0/tcp/5001 
+  if [ ! -f ~/.ipfs/config ]; then
+    npx ipfs init
+    # may need to change in future.
+    npx ipfs config Addresses.API /ip4/0.0.0.0/tcp/5001
+  fi
   npx ipfs daemon > /dev/null 2>&1 &
-  # sleep 5
+  sleep 5
 }
 
 if ipfs_running; then
