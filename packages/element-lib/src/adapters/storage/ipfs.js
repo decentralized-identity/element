ad2b0a1306ce6ed205d4546ec15e9b8d939573e6
gjgd <gjgd@protonmail.com>
2019-07-12 12:35:03 +0100

    Add tests for ipfs

--- packages/element-lib/src/adapters/storage/ipfs.js
+++ packages/element-lib/src/adapters/storage/ipfs.js
@@ -36,8 +36,8 @@ class IpfsStorage {
   }
 
   async read(cid) {
-    const [node] = await resolveValueOrNullInSeconds(this.ipfs.get(cid), 5);
     try {
+      const [node] = await resolveValueOrNullInSeconds(this.ipfs.get(cid), 5);
       return JSON.parse(node.content);
     } catch (e) {
       throw new Error(`Invalid JSON: https://ipfs.io/ipfs/${cid}`);
--- packages/element-lib/src/adapters/storage/ipfs.spec.js
+++ packages/element-lib/src/adapters/storage/ipfs.spec.js
@@ -5,6 +5,10 @@ const testObj = {
   hello: 'world',
 };
 
+const testString = 'string';
+
+const testInteger = 1;
+
 describe('storage.ipfs', () => {
   let storage;
 
@@ -21,16 +25,36 @@ describe('storage.ipfs', () => {
   });
 
   describe('write', () => {
-    it('should write and return content id', async () => {
+    it('should write a JSON and return content id', async () => {
       const cid = await storage.write(testObj);
       expect(cid).toBe('QmNrEidQrAbxx3FzxNt9E6qjEDZrtvzxUVh47BXm55Zuen');
+    });
+
+    it('should write a string and return content id', async () => {
+      const cid = await storage.write(testString);
+      expect(cid).toBe('QmT2PFvRp4VXeBtFRWJUfJpssK6UqTZ5eHpaYQNcEXfNn8');
+    });
+
+    it('should write an integer and return content id', async () => {
+      const cid = await storage.write(testInteger);
+      expect(cid).toBe('QmWYddCPs7uR9EvHNCZzpguVFVNfHc6aM3hPVzPdAEESMc');
     });
   });
 
   describe('read', () => {
-    it('should write and return content id', async () => {
+    it('should read a JSON', async () => {
       const obj = await storage.read('QmNrEidQrAbxx3FzxNt9E6qjEDZrtvzxUVh47BXm55Zuen');
       expect(obj).toEqual(testObj);
+    });
+
+    it('should read a string', async () => {
+      const obj = await storage.read('QmT2PFvRp4VXeBtFRWJUfJpssK6UqTZ5eHpaYQNcEXfNn8');
+      expect(obj).toEqual(testString);
+    });
+
+    it('should read an integer', async () => {
+      const obj = await storage.read('QmWYddCPs7uR9EvHNCZzpguVFVNfHc6aM3hPVzPdAEESMc');
+      expect(obj).toEqual(testInteger);
     });
   });
 });
