f74940a8aaee699b8e8a4eb4514de4067c430efe
gjgd <gjgd@protonmail.com>
2019-07-12 13:54:51 +0100

    Fix reassigning function parameter

--- packages/element-lib/src/sidetree/createTransactionFromRequests.js
+++ packages/element-lib/src/sidetree/createTransactionFromRequests.js
@@ -5,11 +5,8 @@ module.exports = (sidetree) => {
   //   TODO: add batching logic here..
   //   eslint-disable-next-line
   sidetree.createTransactionFromRequests = async requests => {
-    if (!Array.isArray(requests)) {
-      //   eslint-disable-next-line
-      requests = [requests];
-    }
-    const operations = requests.map(requestBody => requestBodyToEncodedOperation(requestBody));
+    const requestArray = Array.isArray(requests) ? requests : [requests];
+    const operations = requestArray.map(requestBody => requestBodyToEncodedOperation(requestBody));
     return operationsToTransaction({
       operations,
       storage: sidetree.storage,
