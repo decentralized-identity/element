4d5956994767759f6377c62fecaaf613a7d6fbff
gjgd <gjgd@protonmail.com>
2019-08-20 16:49:41 +0200

    chore: cleanup dead code

--- packages/element-api/src/express/app.js
+++ packages/element-api/src/express/app.js
@@ -1,35 +1,33 @@
-const express = require('express');
+const express = require("express");
 
-const cors = require('cors');
+const cors = require("cors");
 
 // const morgan = require('morgan');
 // const winston = require('../../src/services/winston');
 
-const swagger = require('../../src/express/swagger');
+const swagger = require("../../src/express/swagger");
 
 const app = express();
 
-const onErrorResponse = require('../../src/express/onErrorResponse');
+const onErrorResponse = require("../../src/express/onErrorResponse");
 
 // Automatically allow cross-origin requests
 app.use(cors({ origin: true }));
 app.use(express.json());
-app.set('sidetree', require('../services/sidetree'));
+app.set("sidetree", require("../services/sidetree"));
 
-// if (process.env.NODE_ENV !== 'testing') {
-//   app.use(morgan('combined', { stream: winston.stream }));
-// }
+app.options("*", cors({ origin: true }));
 
-app.options('*', cors({ origin: true }));
-
-app.use('/api/v1/', require('../../src/express/routes/index'));
+app.use("/api/v1/", require("../../src/express/routes/index"));
 
 // Handle errors.
 app.use(onErrorResponse);
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === "development") {
   // eslint-disable-next-line
-  console.log('\n🔥 API DOCS: http://localhost:5002/element-did/us-central1/main/docs\n');
+  console.log(
+    "\n🔥 API DOCS: http://localhost:5002/element-did/us-central1/main/docs\n"
+  );
 }
 
 // must go last.
