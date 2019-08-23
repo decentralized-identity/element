ac739a49de37e1c3f3c77fb37df70eef5e2ad451
gjgd <gjgd@protonmail.com>
2019-08-23 18:04:46 +0200

    chore: fix getTransactions

--- packages/element-lib/src/sidetree/getTransactions.js
+++ packages/element-lib/src/sidetree/getTransactions.js
@@ -1,43 +1,39 @@
 module.exports = (sidetree) => {
   // eslint-disable-next-line
-  sidetree.getTransactions = async args => {
+  sidetree.getTransactions = async () => {
+    const args = this.arguments || {};
     let transactions;
     let start = 0;
     let end = 'latest';
 
-    if (!args) {
-      transactions = await sidetree.blockchain.getTransactions(start, end);
-    } else {
-      const {
-        since, transactionTimeHash, count, cacheOnly,
-      } = args;
-
-      if (transactionTimeHash) {
-        const blockchainTime = await sidetree.blockchain.getBlockchainTime(transactionTimeHash);
-        start = blockchainTime.time;
-        // if (count) does not work because count = 0 is a valid argument but evaluates as false
-        if (count !== undefined) {
-          end = blockchainTime.time + count;
-        }
+    const {
+      since, transactionTimeHash, count, cacheOnly,
+    } = args;
+    if (transactionTimeHash) {
+      const blockchainTime = await sidetree.blockchain.getBlockchainTime(transactionTimeHash);
+      start = blockchainTime.time;
+      // if (count) does not work because count = 0 is a valid argument but evaluates as false
+      if (count !== undefined) {
+        end = blockchainTime.time + count;
       }
-      if (cacheOnly) {
-        transactions = await sidetree.db.readCollection('element:sidetree:transaction');
-      } else {
-        transactions = await sidetree.blockchain.getTransactions(start, end);
-        await Promise.all(
-          transactions.map(async (transaction) => {
-            await sidetree.db.write(
-              `element:sidetree:transaction:${transaction.transactionTimeHash}`,
-              {
-                type: 'element:sidetree:transaction',
-                ...transaction,
-              },
-            );
-          }),
-        );
-      }
-      transactions = transactions.filter(txn => txn.transactionNumber >= (since || 0));
     }
+    if (cacheOnly) {
+      transactions = await sidetree.db.readCollection('element:sidetree:transaction');
+    } else {
+      transactions = await sidetree.blockchain.getTransactions(start, end);
+      await Promise.all(
+        transactions.map(async (transaction) => {
+          await sidetree.db.write(
+            `element:sidetree:transaction:${transaction.transactionTimeHash}`,
+            {
+              type: 'element:sidetree:transaction',
+              ...transaction,
+            },
+          );
+        }),
+      );
+    }
+    transactions = transactions.filter(txn => txn.transactionNumber >= (since || 0));
 
     return transactions;
   };
