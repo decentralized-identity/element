2606a937e6b7d9674be1bc327901c74060763158
gjgd <gjgd@protonmail.com>
2019-09-05 12:17:00 +0200

    chore: move sync parameter logic to the sync function

--- packages/element-api/src/express/routes/sidetree/index.js
+++ packages/element-api/src/express/routes/sidetree/index.js
@@ -78,11 +78,7 @@ router.post('/requests', async (req, res, next) => {
 router.get('/docs', async (req, res, next) => {
   try {
     const sidetree = req.app.get('sidetree');
-    const currentTime = await sidetree.blockchain.getCurrentTime();
-    const toTransactionTime = currentTime.time;
-    const lastSync = await sidetree.db.read('element:sidetree:sync');
-    const fromTransactionTime = lastSync.syncStopDateTime ? lastSync.lastTransactionTime : 0;
-    await sidetree.sync({ fromTransactionTime, toTransactionTime });
+    await sidetree.sync();
     const result = await sidetree.db.readCollection('element:sidetree:did:documentRecord');
     res.status(200).json(result);
   } catch (e) {
