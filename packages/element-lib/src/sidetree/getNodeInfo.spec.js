e39510b16cf563053634772a17a5d587e301ee9b
Orie Steele <orie@ohs.io>
2019-09-23 13:33:00 -0500

    chore: pr feedback

---
+++ packages/element-lib/src/sidetree/getNodeInfo.spec.js
@@ -0,0 +1,29 @@
+const getLocalSidetree = require('../__tests__/__fixtures__/getLocalSidetree');
+
+jest.setTimeout(10 * 1000);
+
+let sidetree;
+
+beforeAll(async () => {
+  sidetree = await getLocalSidetree('sidetree.getBatchFile');
+});
+
+afterAll(async () => {
+  await sidetree.close();
+});
+
+describe('sidetree.getNodeInfo', () => {
+  it('should return node info', async () => {
+    const info = await sidetree.getNodeInfo();
+    expect(info.ipfs).toEqual({ version: '0.4.22', commit: '', repo: '7' });
+    expect(info.ethereum).toEqual({
+      anchor_contract_address: '0x7C922E2DF8A55F3C27D67AC2cdCcE2fb0BBE8b7B',
+      accounts: ['0x1E228837561e32a6eC1b16f0574D6A493Edc8863'],
+    });
+    expect(info.sidetree).toEqual({
+      BATCH_INTERVAL_SECONDS: 3,
+      BAD_STORAGE_HASH_DELAY_SECONDS: 600,
+      VERBOSITY: 0,
+    });
+  });
+});
