7d4a5f521d6acbc8a18c17513b1113b84bdcd977
Orie Steele <orie@ohs.io>
2019-09-24 14:59:54 -0500

    chore: lint

--- packages/element-lib/src/__tests__/Production.Tests.spec.js
+++ packages/element-lib/src/__tests__/Production.Tests.spec.js
@@ -1,80 +1,74 @@
-const dotenv = require("dotenv");
-const path = require("path");
+const dotenv = require('dotenv');
+const path = require('path');
 
-const element = require("../../index");
+const element = require('../../index');
 
-dotenv.config({ path: path.resolve(__dirname, "../../../../.env") });
+dotenv.config({ path: path.resolve(__dirname, '../../../../.env') });
 
 const config = {
   mnemonic: process.env.ELEMENT_MNEMONIC,
   web3ProviderUrl: process.env.ELEMENT_PROVIDER,
   ipfsApiMultiAddr: process.env.ELEMENT_IPFS_MULTIADDR,
   anchorContractAddress: process.env.ELEMENT_CONTRACT_ADDRESS,
-  couchdb_remote: process.env.ELEMENT_COUCHDB_REMOTE
+  couchdb_remote: process.env.ELEMENT_COUCHDB_REMOTE,
 };
 
 // These tests are for debuging syncing issues in production
-describe.skip("Production Tests", () => {
+describe.skip('Production Tests', () => {
   const blockchain = element.blockchain.ethereum.configure({
     hdPath: "m/44'/60'/0'/0/0",
     mnemonic: config.mnemonic,
     providerUrl: config.web3ProviderUrl,
     // when not defined, a new contract is created.
-    anchorContractAddress: config.anchorContractAddress
+    anchorContractAddress: config.anchorContractAddress,
   });
 
   const storage = element.storage.ipfs.configure({
-    multiaddr: config.ipfsApiMultiAddr
+    multiaddr: config.ipfsApiMultiAddr,
   });
 
   const db = new element.adapters.database.ElementRXDBAdapterWithRemoteSync({
-    name: "production-tests",
-    remote: config.couchdb_remote
+    name: 'production-tests',
+    remote: config.couchdb_remote,
   });
 
   const manager = new element.adapters.storage.StorageManager(db, storage, {
     autoPersist: true,
-    retryIntervalSeconds: 1
+    retryIntervalSeconds: 1,
   });
-  describe("blockchain", () => {
-    it("should have 2 transactions in window", async () => {
+  describe('blockchain', () => {
+    it('should have 2 transactions in window', async () => {
       await blockchain.resolving;
       const txns = await blockchain.getTransactions(6421342, 6421354);
       expect(txns.length).toBe(2);
     });
   });
 
-  describe("storage", () => {
-    it("anchor file is resolvable", async () => {
-      const anchorFile = await storage.read(
-        "QmPyAucuooAEMbdw1uT8veXrLSfBjdNoTqfDRsnt5hrzbS"
-      );
+  describe('storage', () => {
+    it('anchor file is resolvable', async () => {
+      const anchorFile = await storage.read('QmPyAucuooAEMbdw1uT8veXrLSfBjdNoTqfDRsnt5hrzbS');
       expect(anchorFile).toEqual({
-        batchFileHash: "QmZxFCGwiYfutyXDR2TcWs728GrCeAsbVkM7Dazphf35QF",
-        didUniqueSuffixes: ["WJl6h_dz3bvJscpWceWoK7AbD5khKdob0W92zh1sx7M"],
-        merkleRoot:
-          "f0e895ea0791e90f3687498d16824299c9346dab93e6463c182b5b649d3e26ab"
+        batchFileHash: 'QmZxFCGwiYfutyXDR2TcWs728GrCeAsbVkM7Dazphf35QF',
+        didUniqueSuffixes: ['WJl6h_dz3bvJscpWceWoK7AbD5khKdob0W92zh1sx7M'],
+        merkleRoot: 'f0e895ea0791e90f3687498d16824299c9346dab93e6463c182b5b649d3e26ab',
       });
     });
   });
 
-  describe("cloudant", () => {
-    it("can await sync", async () => {
+  describe('cloudant', () => {
+    it('can await sync', async () => {
       const record = await db.read(
-        "element:sidetree:cas-cachable:QmPyAucuooAEMbdw1uT8veXrLSfBjdNoTqfDRsnt5hrzbS"
+        'element:sidetree:cas-cachable:QmPyAucuooAEMbdw1uT8veXrLSfBjdNoTqfDRsnt5hrzbS',
       );
       expect(record).toBe(null);
-      const record1 = await manager.read(
-        "QmPyAucuooAEMbdw1uT8veXrLSfBjdNoTqfDRsnt5hrzbS"
-      );
+      const record1 = await manager.read('QmPyAucuooAEMbdw1uT8veXrLSfBjdNoTqfDRsnt5hrzbS');
       expect(record1).toEqual({
-        batchFileHash: "QmZxFCGwiYfutyXDR2TcWs728GrCeAsbVkM7Dazphf35QF",
-        didUniqueSuffixes: ["WJl6h_dz3bvJscpWceWoK7AbD5khKdob0W92zh1sx7M"],
-        merkleRoot:
-          "f0e895ea0791e90f3687498d16824299c9346dab93e6463c182b5b649d3e26ab"
+        batchFileHash: 'QmZxFCGwiYfutyXDR2TcWs728GrCeAsbVkM7Dazphf35QF',
+        didUniqueSuffixes: ['WJl6h_dz3bvJscpWceWoK7AbD5khKdob0W92zh1sx7M'],
+        merkleRoot: 'f0e895ea0791e90f3687498d16824299c9346dab93e6463c182b5b649d3e26ab',
       });
       const record2 = await db.read(
-        "element:sidetree:cas-cachable:QmPyAucuooAEMbdw1uT8veXrLSfBjdNoTqfDRsnt5hrzbS"
+        'element:sidetree:cas-cachable:QmPyAucuooAEMbdw1uT8veXrLSfBjdNoTqfDRsnt5hrzbS',
       );
       expect(record2.persisted).toBe(true);
       await manager.db.awaitableSync();
