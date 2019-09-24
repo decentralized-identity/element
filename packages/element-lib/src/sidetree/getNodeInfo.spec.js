d75f3858c35611852ac8d00c9a8d99027da75063
Orie Steele <orie@ohs.io>
2019-09-23 20:09:02 -0500

    chore: fix ipfs node version test

--- packages/element-lib/src/sidetree/getNodeInfo.spec.js
+++ packages/element-lib/src/sidetree/getNodeInfo.spec.js
@@ -15,7 +15,9 @@ afterAll(async () => {
 describe('sidetree.getNodeInfo', () => {
   it('should return node info', async () => {
     const info = await sidetree.getNodeInfo();
-    expect(info.ipfs).toEqual({ version: '0.4.22', commit: '', repo: '7' });
+    expect(info.ipfs.version).toBeDefined();
+    expect(info.ipfs.commit).toBeDefined();
+    expect(info.ipfs.repo).toBeDefined();
     expect(info.ethereum).toEqual({
       anchor_contract_address: '0x7C922E2DF8A55F3C27D67AC2cdCcE2fb0BBE8b7B',
       accounts: ['0x1E228837561e32a6eC1b16f0574D6A493Edc8863'],
