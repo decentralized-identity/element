515bd32ab88df0f3cd2af5dcc60ad2fa3faac2ad
gjgd <gjgd@protonmail.com>
2019-11-25 15:12:59 +0100

    chore: create a sidetree-v2 service

--- packages/element-api/src/services/sidetree.js
+++ packages/element-api/src/services/sidetree.js
@@ -9,8 +9,6 @@ const config = getBaseConfig();
 let storage;
 let db;
 
-let sidetree;
-
 const blockchain = element.blockchain.ethereum.configure({
   mnemonic: config.ethereum.mnemonic,
   hdPath: "m/44'/60'/0'/0/0",
@@ -18,62 +16,48 @@ const blockchain = element.blockchain.ethereum.configure({
   anchorContractAddress: config.ethereum.anchor_contract_address,
 });
 
-if (process.env.DOCKER) {
-  db = new element.adapters.database.ElementCouchDBAdapter({
-    name: 'element',
+if (process.env.NODE_ENV === 'testing') {
+  db = new element.adapters.database.ElementRXDBAdapter({
+    name: 'element-did.rxdb.api',
     remote: config.couchdb_remote,
   });
 
   storage = element.storage.ipfs.configure({
     multiaddr: config.ipfs.multiaddr,
   });
-
-  sidetree = new element.SidetreeV2({ db, storage, blockchain });
 } else {
-  if (process.env.NODE_ENV === 'testing') {
-    db = new element.adapters.database.ElementRXDBAdapter({
-      name: 'element-did.rxdb.api',
-      remote: config.couchdb_remote,
-    });
-
-    storage = element.storage.ipfs.configure({
-      multiaddr: config.ipfs.multiaddr,
-    });
-  } else {
-    db = new ElementFirestoreAdapter({
-      firestore,
-    });
-    storage = new element.adapters.storage.StorageManager(
-      db,
-      element.storage.ipfs.configure({
-        multiaddr: config.ipfs.multiaddr,
-      }),
-      {
-        autoPersist: false,
-        retryIntervalSeconds: 5,
-      },
-    );
-  }
-  const serviceBus = new element.adapters.serviceBus.ElementNanoBusAdapter();
-
-  sidetree = new element.Sidetree({
-    blockchain,
-    storage,
-    serviceBus,
-    db,
-    config: {
-      BATCH_INTERVAL_SECONDS: parseInt(
-        config.sidetree.batch_interval_in_seconds,
-        10,
-      ),
-      BAD_STORAGE_HASH_DELAY_SECONDS: parseInt(
-        config.sidetree.bad_storage_hash_delay_in_seconds,
-        10,
-      ),
-      VERBOSITY: parseInt(config.sidetree.verbosity, 10),
-    },
+  db = new ElementFirestoreAdapter({
+    firestore,
   });
+  storage = new element.adapters.storage.StorageManager(
+    db,
+    element.storage.ipfs.configure({
+      multiaddr: config.ipfs.multiaddr,
+    }),
+    {
+      autoPersist: false,
+      retryIntervalSeconds: 5,
+    },
+  );
 }
+const serviceBus = new element.adapters.serviceBus.ElementNanoBusAdapter();
 
+const sidetree = new element.Sidetree({
+  blockchain,
+  storage,
+  serviceBus,
+  db,
+  config: {
+    BATCH_INTERVAL_SECONDS: parseInt(
+      config.sidetree.batch_interval_in_seconds,
+      10,
+    ),
+    BAD_STORAGE_HASH_DELAY_SECONDS: parseInt(
+      config.sidetree.bad_storage_hash_delay_in_seconds,
+      10,
+    ),
+    VERBOSITY: parseInt(config.sidetree.verbosity, 10),
+  },
+});
 
 module.exports = sidetree;
