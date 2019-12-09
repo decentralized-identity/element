38499c3a98a37c61e920b484ec9142a31c9d08ee
gjgd <gjgd@protonmail.com>
2019-12-09 15:45:09 +0100

    test: move sidetreeCreatePayload to the fixtures folder

--- packages/element-lib/src/sidetree-v2/func.spec.js
+++ packages/element-lib/src/sidetree-v2/func.spec.js
@@ -5,9 +5,7 @@ const {
   encodeJson,
 } = require('./func');
 const { MnemonicKeySystem } = require('../../index');
-
-// eslint-disable-next-line
-const sidetreeCreatePayload = { '@context': 'https://w3id.org/did/v1', publicKey: [{ id: '#recoveryKey', type: 'Secp256k1VerificationKey2018', usage: 'recovery', publicKeyHex: '031efa0144680ccb2e691937125472f0277be474ed51e8cf70bf466698b0883154' }, { id: '#signingKey', type: 'Secp256k1VerificationKey2018', usage: 'signing', publicKeyHex: '038861f5dd163bc804b2061bdf7f297c636f52e5e4f2bf219b21a257f84a9ac60a' }], service: [{ type: 'IdentityHub', serviceEndpoint: { '@context': 'schema.identity.foundation/hub', '@type': 'UserServiceEndpoint', instance: ['dummyHubUri1', 'dummyHubUri2'] } }] };
+const sidetreeCreatePayload = require('./__fixtures__/sidetreeCreatePayload.json');
 
 describe('payloadToHash', () => {
   it('should compute the right encodedHash', async () => {
