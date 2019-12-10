c7bb883dba2e2fc5e9d1258a38f2bbb5fbbcf790
gjgd <gjgd@protonmail.com>
2019-12-10 12:58:35 +0100

    test: add tests for batching several transactions

--- packages/element-lib/src/sidetree-v2/protocol/create.spec.js
+++ packages/element-lib/src/sidetree-v2/protocol/create.spec.js
@@ -8,14 +8,12 @@ const {
   decodeJson,
   syncTransaction,
 } = require('../func');
-const element = require('../../../index');
+const { MnemonicKeySystem } = require('../../../index');
 
 const sidetree = getTestSideTree();
 
-const mnemonic = element.MnemonicKeySystem.generateMnemonic();
-const mks = new element.MnemonicKeySystem(mnemonic);
-
 describe('create', () => {
+  const mks = new MnemonicKeySystem(MnemonicKeySystem.generateMnemonic());
   let createPayload;
   let transaction;
   let anchorFile;
@@ -64,6 +62,83 @@ describe('create', () => {
     const did = `did:elem:${didUniqueSuffix}`;
     expect(didDocument.id).toBe(did);
     const decodedPayload = decodeJson(createPayload.payload);
+    expect(didDocument['@context']).toBe(decodedPayload['@context']);
+    expect(didDocument.publicKey).toEqual(decodedPayload.publicKey);
+  });
+});
+
+describe('create batch', () => {
+  const mks = new MnemonicKeySystem(MnemonicKeySystem.generateMnemonic());
+  let createPayload1;
+  let createPayload2;
+  let didUniqueSuffix1;
+  let didUniqueSuffix2;
+  let batch;
+  let transaction;
+  let anchorFile;
+  let batchFile;
+
+  const getPayloadForKeyIndex = async (index) => {
+    const primaryKey = await mks.getKeyForPurpose('primary', index);
+    const recoveryKey = await mks.getKeyForPurpose('recovery', index);
+    const didDocumentModel = getDidDocumentModel(primaryKey.publicKey, recoveryKey.publicKey);
+    return getCreatePayload(didDocumentModel, primaryKey);
+  };
+
+  beforeAll(async () => {
+    createPayload1 = await getPayloadForKeyIndex(0);
+    createPayload2 = await getPayloadForKeyIndex(1);
+    didUniqueSuffix1 = getDidUniqueSuffix(createPayload1);
+    didUniqueSuffix2 = getDidUniqueSuffix(createPayload2);
+    batch = [createPayload1, createPayload2];
+  });
+
+  it('should submit a batched transaction', async () => {
+    transaction = await create(sidetree)(batch);
+    expect(transaction.anchorFileHash).toBeDefined();
+  });
+
+  it('should publish anchorFile to IPFS', async () => {
+    anchorFile = await sidetree.storage.read(transaction.anchorFileHash);
+    expect(anchorFile.batchFileHash).toBeDefined();
+    expect(anchorFile.didUniqueSuffixes).toEqual([didUniqueSuffix1, didUniqueSuffix2]);
+    expect(anchorFile.merkleRoot).toBeDefined();
+  });
+
+  it('should publish batchFile to IPFS', async () => {
+    batchFile = await sidetree.storage.read(anchorFile.batchFileHash);
+    expect(batchFile.operations).toBeDefined();
+  });
+
+  it('should contain the correct operations', async () => {
+    const operations = batchFileToOperations(batchFile);
+    expect(operations).toHaveLength(2);
+    expect(operations[0].decodedOperation).toEqual(createPayload1);
+    expect(operations[1].decodedOperation).toEqual(createPayload2);
+  });
+
+  it('should not return the did before sync is called', async () => {
+    const didDocument1 = await resolve(sidetree)(didUniqueSuffix1);
+    expect(didDocument1).not.toBeDefined();
+    const didDocument2 = await resolve(sidetree)(didUniqueSuffix2);
+    expect(didDocument2).not.toBeDefined();
+  });
+
+  it('should resolve the first did when the observer synced the transaction', async () => {
+    await syncTransaction(sidetree, transaction);
+    const didDocument = await resolve(sidetree)(didUniqueSuffix1);
+    const did = `did:elem:${didUniqueSuffix1}`;
+    expect(didDocument.id).toBe(did);
+    const decodedPayload = decodeJson(createPayload1.payload);
+    expect(didDocument['@context']).toBe(decodedPayload['@context']);
+    expect(didDocument.publicKey).toEqual(decodedPayload.publicKey);
+  });
+
+  it('should resolve the second did when the observer synced the transaction', async () => {
+    const didDocument = await resolve(sidetree)(didUniqueSuffix2);
+    const did = `did:elem:${didUniqueSuffix2}`;
+    expect(didDocument.id).toBe(did);
+    const decodedPayload = decodeJson(createPayload2.payload);
     expect(didDocument['@context']).toBe(decodedPayload['@context']);
     expect(didDocument.publicKey).toEqual(decodedPayload.publicKey);
   });
