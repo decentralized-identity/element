6bd4fb29b24f1859752afca4dc8e176239484e6e
gjgd <gjgd@protonmail.com>
2019-12-11 11:56:42 +0100

    chore: add func to sidetree

--- packages/element-lib/src/sidetree-v2/index.js
+++ packages/element-lib/src/sidetree-v2/index.js
@@ -1,17 +1,22 @@
 const { resolve, sync, batchWrite } = require('./protocol');
 const BatchScheduler = require('./protocol/BatchScheduler');
 const op = require('./op');
+const func = require('./func');
 const OperationQueue = require('./operationQueue');
 
 class Sidetree {
   constructor({ db, blockchain, storage }) {
     const operationQueue = new OperationQueue(db);
+    // Utils for sidetree
     this.blockchain = blockchain;
     this.storage = storage;
     this.db = db;
     this.op = op;
-    this.resolve = resolve(this);
+    this.func = func;
+    // Observer
     this.sync = sync(this);
+    // Resolver
+    this.resolve = resolve(this);
     // Batching
     this.operationQueue = operationQueue;
     this.batchWrite = batchWrite(this);
