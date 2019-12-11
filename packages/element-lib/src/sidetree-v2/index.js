62fdd57d5f9d6a45f20ee968ad784497d8935ecc
gjgd <gjgd@protonmail.com>
2019-12-11 11:20:44 +0100

    chore: update sidetree class

--- packages/element-lib/src/sidetree-v2/index.js
+++ packages/element-lib/src/sidetree-v2/index.js
@@ -1,15 +1,21 @@
 const { resolve, sync, batchWrite } = require('./protocol');
+const BatchScheduler = require('./protocol/BatchScheduler');
 const op = require('./op');
+const OperationQueue = require('./operationQueue');
 
 class Sidetree {
   constructor({ db, blockchain, storage }) {
+    const operationQueue = new OperationQueue(db);
     this.blockchain = blockchain;
     this.storage = storage;
     this.db = db;
+    this.op = op;
     this.resolve = resolve(this);
     this.sync = sync(this);
+    // Batching
+    this.operationQueue = operationQueue;
     this.batchWrite = batchWrite(this);
-    this.op = op;
+    this.batchScheduler = new BatchScheduler(this);
   }
 }
 
