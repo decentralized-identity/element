a2dac60de6caf7f4bf3804a305e9c853527d79d2
gjgd <gjgd@protonmail.com>
2019-12-17 18:37:33 +0100

    chore: implement createAddKeyRequest

--- packages/element-app/src/redux/sidetreeWallet/handlers.js
+++ packages/element-app/src/redux/sidetreeWallet/handlers.js
@@ -7,20 +7,56 @@ export default withHandlers({
   getMyDidUniqueSuffix: ({ getKey }) => async () => {
     const primaryKey = getKey('#primary');
     const recoveryKey = getKey('#recovery');
-    const didDocumentModel = op.getDidDocumentModel(primaryKey.publicKey, recoveryKey.publicKey);
-    const createPayload = await op.getCreatePayload(didDocumentModel, primaryKey);
+    const didDocumentModel = op.getDidDocumentModel(
+      primaryKey.publicKey,
+      recoveryKey.publicKey,
+    );
+    const createPayload = await op.getCreatePayload(
+      didDocumentModel,
+      primaryKey,
+    );
     const didUniqueSuffix = func.getDidUniqueSuffix(createPayload);
     return didUniqueSuffix;
   },
   createDIDRequest: ({ getKey }) => async () => {
     const primaryKey = getKey('#primary');
-    const didDocumentModel = op.getDidDocumentModel(primaryKey.publicKey, primaryKey.publicKey);
-    const createPayload = await op.getCreatePayload(didDocumentModel, primaryKey);
+    const didDocumentModel = op.getDidDocumentModel(
+      primaryKey.publicKey,
+      primaryKey.publicKey,
+    );
+    const createPayload = await op.getCreatePayload(
+      didDocumentModel,
+      primaryKey,
+    );
     return createPayload;
   },
-  createAddKeyRequest: ({ getKey }) => async (key, myDidDocument, previousOperationHash) => {
+  createAddKeyRequest: ({ getKey }) => async (
+    kid,
+    newPublicKey,
+    didUniqueSuffix,
+    operationHash,
+  ) => {
+    const lastOperation = {
+      didUniqueSuffix,
+      operation: { operationHash },
+    };
+    const primaryKey = getKey('#primary');
+    const payload = await op.getUpdatePayloadForAddingAKey(
+      lastOperation,
+      kid,
+      'signing',
+      newPublicKey,
+      primaryKey.privateKey,
+    );
+    console.log({ payload });
+    return payload;
   },
-  createRemoveKeyRequest: ({ getKey }) => async (kid, didUniqueSuffix, operationHash) => {
+  // TODO remove didUniqueSuffix
+  createRemoveKeyRequest: ({ getKey }) => async (
+    kid,
+    didUniqueSuffix,
+    operationHash,
+  ) => {
     const lastOperation = {
       didUniqueSuffix,
       operation: { operationHash },
