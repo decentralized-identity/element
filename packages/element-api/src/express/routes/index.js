149be7dbdf7b694f04062f2ae52bd391cb560423
gjgd <gjgd@protonmail.com>
2019-12-19 15:15:10 +0100

    chore: remove sidetree-v2 routes

--- packages/element-api/src/express/routes/index.js
+++ packages/element-api/src/express/routes/index.js
@@ -4,6 +4,5 @@ const router = express.Router();
 
 router.use('/version', require('./version'));
 router.use('/sidetree', require('./sidetree'));
-router.use('/sidetree-v2', require('./sidetree-v2'));
 
 module.exports = router;
