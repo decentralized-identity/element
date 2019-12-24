c847073222081b0c2ba772fc363d8704e1e00ebb
Orie Steele <orie@ohs.io>
2019-12-24 14:43:31 -0600

    chore: fix lint errors

--- packages/element-lib/src/__tests__/__fixtures__/sidetreeTestUtils.js
+++ packages/element-lib/src/__tests__/__fixtures__/sidetreeTestUtils.js
@@ -6,13 +6,15 @@ const { getLastOperation } = require('../test-utils');
 const actors = {};
 const getActorByIndex = index => actors[Object.keys(actors)[index]];
 
-const generateActors = async (count) => {
+const generateActors = async count => {
+  // eslint-disable-next-line
   for (let i = 0; i < count; i++) {
     const mks = new element.MnemonicKeySystem(element.MnemonicKeySystem.generateMnemonic());
     const primaryKey = mks.getKeyForPurpose('primary', 0);
     const recoveryKey = mks.getKeyForPurpose('recovery', 0);
     const didDocumentModel = element.op.getDidDocumentModel(
-      primaryKey.publicKey, recoveryKey.publicKey,
+      primaryKey.publicKey,
+      recoveryKey.publicKey,
     );
     // eslint-disable-next-line no-await-in-loop
     const createPayload = await element.op.getCreatePayload(didDocumentModel, primaryKey);
@@ -41,12 +43,13 @@ const generateActors = async (count) => {
   return actors;
 };
 
-const createByActorIndex = async (actorIndex) => {
+const createByActorIndex = async actorIndex => {
   const actor = getActorByIndex(actorIndex);
   const primaryKey = actor.mks.getKeyForPurpose('primary', 0);
   const recoveryKey = actor.mks.getKeyForPurpose('recovery', 0);
   const didDocumentModel = element.op.getDidDocumentModel(
-    primaryKey.publicKey, recoveryKey.publicKey,
+    primaryKey.publicKey,
+    recoveryKey.publicKey,
   );
   return element.op.getCreatePayload(didDocumentModel, primaryKey);
 };
@@ -56,9 +59,7 @@ const assertCreateSucceeded = async (sidetree, actorIndex) => {
   const did = `did:elem:${actor.didUniqueSuffix}`;
   const didDoc = await sidetree.resolve(did, true);
   expect(didDoc.id).toBe(did);
-  expect(didDoc.publicKey[0].publicKeyHex).toBe(
-    actor.mks.getKeyForPurpose('primary', 0).publicKey,
-  );
+  expect(didDoc.publicKey[0].publicKeyHex).toBe(actor.mks.getKeyForPurpose('primary', 0).publicKey);
   expect(didDoc.publicKey[1].publicKeyHex).toBe(
     actor.mks.getKeyForPurpose('recovery', 0).publicKey,
   );
@@ -73,7 +74,11 @@ const updateByActorIndex = async (sidetree, actorIndex) => {
   const lastOperation = await getLastOperation(sidetree, didUniqueSuffix);
   const newKey = actor.mks.getKeyForPurpose('primary', 10);
   return element.op.getUpdatePayloadForAddingAKey(
-    lastOperation, '#newKey', 'signing', newKey.publicKey, actor.primaryKey.privateKey,
+    lastOperation,
+    '#newKey',
+    'signing',
+    newKey.publicKey,
+    actor.primaryKey.privateKey,
   );
 };
 
@@ -83,10 +88,13 @@ const recoverByActorIndex = async (sidetree, actorIndex) => {
   const newPrimaryPublicKey = actor.mks.getKeyForPurpose('primary', 20).publicKey;
   const newRecoveryPublicKey = actor.mks.getKeyForPurpose('recovery', 20).publicKey;
   const didDocumentModel = sidetree.op.getDidDocumentModel(
-    newPrimaryPublicKey, newRecoveryPublicKey,
+    newPrimaryPublicKey,
+    newRecoveryPublicKey,
   );
   return sidetree.op.getRecoverPayload(
-    didUniqueSuffix, didDocumentModel, actor.recoveryKey.privateKey,
+    didUniqueSuffix,
+    didDocumentModel,
+    actor.recoveryKey.privateKey,
   );
 };
 
@@ -112,14 +120,17 @@ const assertRecoverSucceeded = async (sidetree, actorIndex) => {
 };
 
 // FIXME jit by default
-const deactivateByActorIndex = async (actorIndex) => {
+const deactivateByActorIndex = async actorIndex => {
   const actor = getActorByIndex(actorIndex);
   const { didUniqueSuffix } = getActorByIndex(actorIndex);
   return element.op.getDeletePayload(didUniqueSuffix, actor.recoveryKey.privateKey);
 };
 
 const assertDeactivateSucceeded = async (sidetree, actorIndex) => {
-  const didDoc = await sidetree.resolve(`did:elem:${getActorByIndex(actorIndex).didUniqueSuffix}`, true);
+  const didDoc = await sidetree.resolve(
+    `did:elem:${getActorByIndex(actorIndex).didUniqueSuffix}`,
+    true,
+  );
   expect(didDoc).not.toBeDefined();
 };
 
