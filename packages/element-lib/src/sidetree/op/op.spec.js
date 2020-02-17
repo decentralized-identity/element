bb243de7b4ff37946264bd20082f726e456d0c72
gjgd <gjgd@protonmail.com>
2020-02-17 17:08:45 +0700

    chore: fix op test

--- packages/element-lib/src/sidetree/op/op.spec.js
+++ packages/element-lib/src/sidetree/op/op.spec.js
@@ -3,9 +3,10 @@ const element = require('../../../index');
 
 describe('op', () => {
   it('getNewWallet', async () => {
-    const wallet = await element.op.getNewWallet();
-    expect(Object.values(wallet.keys)[1].tags.length).toBe(3);
+    const wallet = await element.op.getNewWallet('did:methodName');
     expect(wallet).toBeDefined();
+    expect(Object.values(wallet.keys)[1].tags.length).toBe(3);
+    expect(Object.values(wallet.keys)[0].tags[2]).toContain('did:methodName');
 
     const didDoc = element.op.walletToInitionalDIDDoc(wallet);
     expect(isDidDocumentModelValid(didDoc)).toBeTruthy();
