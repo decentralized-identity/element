e50b2b55e59528cb92dfd1c005f91e481c162947
Orie Steele <orie@transmute.industries>
2020-07-09 20:44:38 -0500

    test

--- packages/element-lib/src/schema/__fixtures__/index.js
+++ packages/element-lib/src/schema/__fixtures__/index.js
@@ -1,12 +1,12 @@
 /* eslint-disable global-require */
 const keys = {
-  'did:key:z6Mkozt95WhH9chvYaPTTsd1FzMbXc1cuvo1hfiZodGNd9Gs': require('./keys/did:key:z6Mkozt95WhH9chvYaPTTsd1FzMbXc1cuvo1hfiZodGNd9Gs.json'),
+  'did:key:z6Mkozt95WhH9chvYaPTTsd1FzMbXc1cuvo1hfiZodGNd9Gs': require('./keys/key.json'),
 };
 
 const didDocs = {
-  'did:key:z6Mkozt95WhH9chvYaPTTsd1FzMbXc1cuvo1hfiZodGNd9Gs': require('./didDocs/did:key:z6Mkozt95WhH9chvYaPTTsd1FzMbXc1cuvo1hfiZodGNd9Gs.json'),
-  'did:work:2UUHQCd4psvkPLZGnWY33L': require('./didDocs/did:work:2UUHQCd4psvkPLZGnWY33L.json'),
-  'did:github:OR13': require('./didDocs/did:github:OR13.json'),
+  'did:key:z6Mkozt95WhH9chvYaPTTsd1FzMbXc1cuvo1hfiZodGNd9Gs': require('./didDocs/key.json'),
+  'did:work:2UUHQCd4psvkPLZGnWY33L': require('./didDocs/work.json'),
+  'did:github:OR13': require('./didDocs/github.json'),
 };
 
 module.exports = {
